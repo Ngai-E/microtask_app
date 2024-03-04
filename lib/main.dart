dbdb09da73613feb866bbaabf891cd2e33894a37
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-03-04 05:41:16 +0100

    learn how to manipulate dialog

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,44 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) => MaterialApp(home: MyHomePage());
 }
 
-class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState()  => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage>{
   String _inputText = '';
+
+  void _showInputDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+                  TextEditingController controller = TextEditingController();
+                  return AlertDialog(
+                            title: Text("Enter you name"),
+                            content: TextField(controller: controller, decoration: InputDecoration(hintText: "Enter some text"),),
+                            actions: <Widget>[
+                              TextButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancel')),
+                              TextButton(onPressed: ()   {
+                                setState(() {
+                                  _inputText = controller.text;
+                                });
+                                Navigator.of(context).pop();
+                              }, child: Text("Save"))
+                            ],
+                  );
+        });
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: Text('Creating Stateful Widgets'),),
-    body: Center(
-      child: TextField(
-        decoration: InputDecoration(hintText: "Enter some text here!"),
-        onChanged: (value) {
-          setState(() {
-            _inputText = value;
-          });
-        },
-      ),
-    ),
-    bottomSheet: Container(
-      alignment: Alignment.center,
-      height: 50,
-      child: Text('you entered $_inputText'),
+    appBar: AppBar(title: Text("Testing Alert"),),
+    body: Center(child: Text(_inputText.isEmpty ? 'Tap the icon  to enter text' : _inputText),),
+    floatingActionButton: FloatingActionButton(
+      onPressed: _showInputDialog,
+      tooltip: 'Enter text',
+      child: const Icon(Icons.edit),
     ),
   );
+
 }
