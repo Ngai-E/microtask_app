1764f0206c51354054bd8420f32bde314333ebdc
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-03-04 06:14:29 +0100

    Listview with List builder

--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,25 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final items = ['item1', 'item2', 'item3'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'learning list Builder',
       home: Scaffold(
-        appBar: AppBar(title: Text('List View Practice'),),
-        body: ListView(
-          children: [
-            ListTile(
-              title: Text('Person one'),
-              subtitle: Text('person1@gmail.com'),
-              trailing: Icon(Icons.arrow_forward),
-              leading: Icon(Icons.person),
-              onTap: () => print('person 1 was tapped'),
-            ),ListTile(
-              title: Text('Person three'),
-              subtitle: Text('person3@gmail.com'),
-              trailing: Icon(Icons.arrow_forward),
-              leading: Icon(Icons.person),
-              onTap: () => print('person 3 was tapped'),
-            ),ListTile(
-              title: Text('Person four'),
-              subtitle: Text('person4@gmail.com'),
-              trailing: Icon(Icons.arrow_forward),
-              leading: Icon(Icons.person),
-              onTap: () => print('person 4 was tapped'),
-            ),
-            ListTile(
-              title: Text('Person two'),
-              subtitle: Text('person2@gmail.com'),
-              trailing: Icon(Icons.arrow_forward),
-              leading: Icon(Icons.person),
-              onTap: () => print('person 2 was tapped'),
-            )
-          ],
+        appBar: AppBar(title: Text('List View Builder'),),
+        body: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+              return ListTile(
+                title: Text(item),
+                onTap: () => print('item $item was tapped'),
+              );
+            }
         ),
       ),
     );
   }
 }
+
