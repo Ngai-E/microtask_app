3eceabd96706e4c53b93c5a49602656600e99323
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-03-04 05:56:19 +0100

    learn listview

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(home: MyHomePage());
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState()  => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>{
-  String _inputText = '';
-
-  void _showInputDialog() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-                  TextEditingController controller = TextEditingController();
-                  return AlertDialog(
-                            title: Text("Enter you name"),
-                            content: TextField(controller: controller, decoration: InputDecoration(hintText: "Enter some text"),),
-                            actions: <Widget>[
-                              TextButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancel')),
-                              TextButton(onPressed: ()   {
-                                setState(() {
-                                  _inputText = controller.text;
-                                });
-                                Navigator.of(context).pop();
-                              }, child: Text("Save"))
-                            ],
-                  );
-        });
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('List View Practice'),),
+        body: ListView(
+          children: [
+            ListTile(
+              title: Text('Person one'),
+              subtitle: Text('person1@gmail.com'),
+              trailing: Icon(Icons.arrow_forward),
+              leading: Icon(Icons.person),
+              onTap: () => print('person 1 was tapped'),
+            ),ListTile(
+              title: Text('Person three'),
+              subtitle: Text('person3@gmail.com'),
+              trailing: Icon(Icons.arrow_forward),
+              leading: Icon(Icons.person),
+              onTap: () => print('person 3 was tapped'),
+            ),ListTile(
+              title: Text('Person four'),
+              subtitle: Text('person4@gmail.com'),
+              trailing: Icon(Icons.arrow_forward),
+              leading: Icon(Icons.person),
+              onTap: () => print('person 4 was tapped'),
+            ),
+            ListTile(
+              title: Text('Person two'),
+              subtitle: Text('person2@gmail.com'),
+              trailing: Icon(Icons.arrow_forward),
+              leading: Icon(Icons.person),
+              onTap: () => print('person 2 was tapped'),
+            )
+          ],
+        ),
+      ),
+    );
   }
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: Text("Testing Alert"),),
-    body: Center(child: Text(_inputText.isEmpty ? 'Tap the icon  to enter text' : _inputText),),
-    floatingActionButton: FloatingActionButton(
-      onPressed: _showInputDialog,
-      tooltip: 'Enter text',
-      child: const Icon(Icons.edit),
-    ),
-  );
-
 }
