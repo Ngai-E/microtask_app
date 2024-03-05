ec1bcd59740633754a473ca86d8f72307385a8ac
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-03-05 05:04:00 +0100

    todo list part 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,65 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  final items = ['item1', 'item2', 'item3'];
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'learning list Builder',
-      home: Scaffold(
-        appBar: AppBar(title: Text('List View Builder'),),
-        body: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              final item = items[index];
-              return ListTile(
-                title: Text(item),
-                onTap: () => print('item $item was tapped'),
-              );
-            }
-        ),
-      ),
-    );
+    return MaterialApp(home: MyHomePage(),);
   }
 }
 
+class MyHomePage extends StatefulWidget{
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  List<String> _todos = ['task 1', 'task 2', 'task 3'];
+  void _addTodo() {
+    showDialog(context: context,
+        builder: (BuildContext context) {
+            String newTodo = '';
+            return AlertDialog(
+              title: Text('Enter a new text below'),
+              content: TextField(
+                onChanged: (value) {
+                  newTodo = value;
+                },
+              ),
+              actions: <Widget>[
+                TextButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancel')),
+                TextButton(
+                    onPressed: (){
+                        setState(() {
+                          _todos.add(newTodo);
+                        });
+                        Navigator.of(context).pop();
+                        },
+                    child: Text('Sumit'))
+              ],
+            );
+        });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo List'),),
+      body: ListView.builder(
+        itemCount: _todos.length,
+        itemBuilder: (context, index) {
+          final todo = _todos[index];
+          return ListTile(
+            title: Text(todo),
+            onTap: () {
+              setState(() {
+                _todos.removeAt(index);
+              });
+            },);
+        }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTodo,
+        child: Icon(Icons.add),
+      ),
+      );
+  }
+}
