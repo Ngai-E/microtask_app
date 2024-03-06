f96683a83ead4e53dbae8a0a2ecfcfa57d4c0226
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-03-06 21:58:31 +0100

    send data back to parent screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,69 @@
-
 import 'package:flutter/material.dart';
 
-class Todo {
-  final String title;
-  final String description;
-
-  const Todo(this.title, this.description);
+void main() {
+  runApp(MyApp());
 }
 
-List<Todo> todos =  [
-  const Todo('todo item 1', 'todo item 1 description'),
-  const Todo('todo item 2', 'todo item 2 description'),
-  const Todo('todo item 3', 'todo item 3 description'),
-];
-
-void main() => runApp(MaterialApp(title: 'Todo app', home: TodosScreen( todos: todos,),));
-
-class TodosScreen extends StatelessWidget{
-  const TodosScreen({super.key, required this.todos});
-  final List<Todo> todos;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('TODO LIST'),),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(todos[index].title),
-          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsScreen(todo: todos[index])))
-          ),
-        ),
+    return MaterialApp(
+      home: HomeScreen(),
       );
   }
 }
 
-class DetailsScreen extends StatelessWidget{
-  final Todo todo;
-  const DetailsScreen({super.key, required this.todo});
+class HomeScreen  extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _selectedOption = 'None';
+
+  void _selectOption() async {
+    var result = await Navigator.push(context, MaterialPageRoute(builder: (context) => OptionsScreen()));
+
+    setState(() {
+      _selectedOption = result ?? 'NONE';
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Details',
-      home: Scaffold(
-        appBar: AppBar(title: Text(todo.title),),
-        body: Center(child: Text(todo.description),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Selected Option: $_selectedOption'),
+            ElevatedButton(onPressed: _selectOption, child: Text('Select Option')),
+          ],
+        ),
       ),
     );
   }
 }
+
+class OptionsScreen  extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Options Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(onPressed: () => Navigator.pop(context, 'option 1'), child: Text('Option 1')),
+            ElevatedButton(onPressed: () => Navigator.pop(context, 'option 2'), child: Text('Option 12')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
