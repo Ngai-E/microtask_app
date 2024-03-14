0ab64dd0c2c8a5a6beed7387469f225b1a87b920
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-03-14 22:06:32 +0100

    learn form

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,57 @@
 import 'package:flutter/material.dart';
 
-class Todo {
-  final String title;
-  final String description;
-
-  const Todo(this.title, this.description);
-}
-
-final List<Todo> _todos = [
-  const Todo('Task one', 'take out the trash'),
-  const Todo('Task two', 'paint the house')
-];
-
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    final String appBarTitle = 'Form Styling Demo';
     return MaterialApp(
-      title: 'Todo App',
-      home: TasksScreen( todos: _todos,),
-    );
-  }
-}
-
-class TasksScreen extends StatefulWidget{
-  final List<Todo> todos;
-
-  const TasksScreen({super.key, required this.todos});
-
-  @override
-  State<StatefulWidget> createState() {
-    return _TasksScreenState();
-  }
-}
-
-class _TasksScreenState  extends State<TasksScreen>{
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('My tasks'),),
-      body: ListView.builder(
-        itemCount: _todos.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(_todos[index].title),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => DetailsScreen(todo: _todos[index])));
-            },
-          );
-        },
+      title: 'Material form field test',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        fontFamily: 'Roboto'
       ),
-      floatingActionButton:  FloatingActionButton(
-        onPressed: _createNewTask,
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  _createNewTask()  async {
-    Todo? result =  await showDialog(context: context, builder: (context) {
-      String? title;
-      String? description;
-      return AlertDialog(
-        title: Text('Create New Task'),
-        content: SizedBox(
-          height: 100,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget> [
-            TextField(
-              decoration: InputDecoration(hintText: 'Enter title'),
-              onChanged: (value) => title = value,
-            ),
-            TextField(
-              decoration: InputDecoration(hintText: 'Enter description'),
-              onChanged: (value) => description = value,
-            ),
-          ],
-        ),),
-        actions: <Widget>[
-          TextButton(onPressed: () => Navigator.pop(context), child: Text('Cancel')),
-          TextButton(onPressed: () {
-            if (title != null && description != null) {
-              Navigator.pop(context, Todo(title!, description!));
-            }
-          }, child: Text('Save'))
-        ],
-      );
-    });
-
-    if (result != null) {
-      setState(() {
-        _todos.add(result);
-      });
-    }
-
-  }
-}
-
-class DetailsScreen extends StatelessWidget{
-  final Todo todo;
-
-  const DetailsScreen({super.key, required this.todo});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Details Screen',
       home: Scaffold(
-        appBar: AppBar(title: Text(this.todo.title),),
-        body: Center(
-          child: Text(this.todo.description),
-        ),
+        appBar: AppBar(title: Text(appBarTitle),),
+        body: const MyFormScreen(),
       ),
     );
   }
 }
 
+class MyFormScreen extends StatelessWidget{
+  const MyFormScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 40,),
+          TextField(
+            style: TextStyle(color: Colors.blue, fontSize: 25),
+            decoration: InputDecoration(
+                hintText: 'enter your name',
+                prefixIcon: Icon(Icons.person, size: 30,)),),
+          const SizedBox(height: 16,),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Name',
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))),),
+          const SizedBox(height: 40,),
+          ElevatedButton(
+              onPressed: () => {},
+              child: const Text('Submit'),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),)
+        ],
+      ),
+    );
+  }
+}
