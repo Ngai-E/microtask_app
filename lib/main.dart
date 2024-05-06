b755ff82e0f09661826bee87c9056a8e8db06ecc
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-05-06 08:05:23 +0100

    begin reading and writing to file

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,63 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+  MaterialApp(title: 'demo', home: FlutterDemo(storage: CounterStorage()),)
+);
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+class FlutterDemo extends StatefulWidget{
+  final CounterStorage storage;
+  const FlutterDemo({super.key, required this.storage})
+
+  @override
+  _FlutterDemoState createState() => _FlutterDemoState();
+}
+
+class _FlutterDemoState  extends State<FlutterDemo>{
+  int _counter = 0;
   @override
   Widget build(BuildContext context) {
-    final String appBarTitle = 'Form Styling Demo';
-    return MaterialApp(
-      title: 'Material form field test',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        fontFamily: 'Roboto'
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: Text(appBarTitle),),
-        body: const MyFormScreen(),
-      ),
-    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readCounter().then((value) => {
+      setState(() {
+        _counter = value;
+      })
+    });
   }
 }
 
-class MyFormScreen extends StatelessWidget{
-  const MyFormScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.all(24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(height: 40,),
-          TextField(
-            style: TextStyle(color: Colors.blue, fontSize: 25),
-            decoration: InputDecoration(
-                hintText: 'enter your name',
-                prefixIcon: Icon(Icons.person, size: 30,)),),
-          const SizedBox(height: 16,),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Name',
-            filled: true,
-            fillColor: Colors.white,
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(12))),),
-          const SizedBox(height: 40,),
-          ElevatedButton(
-              onPressed: () => {},
-              child: const Text('Submit'),
-            style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),)
-        ],
-      ),
-    );
+class CounterStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localFile;
+    return File('$path/counter.txt');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+      final contents = await file.readAsString();
+      return int.parse(contents);
+    } catch (err) {
+      return 0; //this is the first time app is opened
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+    return file.writeAsString('$counter');
   }
 }
