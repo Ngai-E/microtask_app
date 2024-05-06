cd5a8fe40dbb8563f7c40778454c28ff48605f51
Ngai Elizabeth <elizabeth@hivecpq.com>
2024-05-06 23:02:55 +0100

    update http get using form

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> fetchAlbum() async {
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/2'));
+Future<Album> fetchAlbum(int albumId) async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/$albumId'));
   if (response.statusCode == 200) {
     return Album.fromJson(jsonDecode(response.body));
   } else {
@@ -44,23 +44,44 @@ class FlutterDemo extends StatefulWidget{
 
 class _FlutterDemoState  extends State<FlutterDemo>{
   late Future<Album> futureAlbum;
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void _fetchAlbum() {
+    int albumId = int.tryParse(_textEditingController.text) ?? 1;
+    setState(() {
+      futureAlbum = fetchAlbum(albumId);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Http Example'),),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: FutureBuilder<Album> (builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Text(snapshot.data!.title);
-            } else if (snapshot.hasError){
-              return Text('${snapshot.error}');
-            }
-            return const CircularProgressIndicator();
-          }, future: futureAlbum,)
-        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _textEditingController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Album Id Number'),
+            ),
+            ElevatedButton(
+                onPressed: _fetchAlbum,
+                child: const Text('Submit')),
+            FutureBuilder<Album>(
+                future: futureAlbum,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data!.title);
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
+                  return const CircularProgressIndicator();
+                }
+            )
+          ],
+        )
 
       )
     );
@@ -69,7 +90,7 @@ class _FlutterDemoState  extends State<FlutterDemo>{
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    futureAlbum = fetchAlbum(1);
   }
 
 }
